19c1b64a4d767dca9507a7c3116de651cfb98149
Tryfon Emman <tremmanouilidis@gmail.com>
2016-06-13 08:35:32 +0300

    Completed static pages. Initial commit for rest api

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-router.get('/', ctrlMain.index );
+router.get('/', ctrlLocations.index );
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
